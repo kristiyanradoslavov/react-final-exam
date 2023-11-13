0cad9f6473e0a337f35fb689e279f00184e988f9
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-11-13 22:44:55 +0200

    commit new changes

--- gaming/src/App.jsx
+++ gaming/src/App.jsx
@@ -1,248 +1,28 @@
+import Features from "./components/Features"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Trending from "./components/Trending"
+import Welcome from "./components/Welcome"
+
 
 function App() {
 
     return (
         <>
             {/* Preloader */}
-            <div id="js-preloader" className="js-preloader">
-                <div className="preloader-inner">
-                    <span className="dot" />
-                    <div className="dots">
-                        <span />
-                        <span />
-                        <span />
-                    </div>
-                </div>
-            </div>
-            {/* ***** Preloader End ***** */}
 
             {/* ***** Header Area Start ***** */}
-            <header className="header-area header-sticky">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            <nav className="main-nav">
-                                {/* ***** Logo Start ***** */}
-                                <a href="index.html" className="logo">
-                                    <img src="assets/images/logo.png" alt="" style={{ width: 158 }} />
-                                </a>
-                                {/* ***** Logo End ***** */}
-                                {/* ***** Menu Start ***** */}
-                                <ul className="nav">
-                                    <li>
-                                        <a href="index.html" className="active">
-                                            Home
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="shop.html">Our Shop</a>
-                                    </li>
-                                    <li>
-                                        <a href="product-details.html">Product Details</a>
-                                    </li>
-                                    <li>
-                                        <a href="contact.html">Contact Us</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Sign In</a>
-                                    </li>
-                                </ul>
-                                <a className="menu-trigger">
-                                    <span>Menu</span>
-                                </a>
-                                {/* ***** Menu End ***** */}
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </header>
+
+            <Header />
+
             {/* ***** Header Area End ***** */}
-            <div className="main-banner">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-6 align-self-center">
-                            <div className="caption header-text">
-                                <h6>Welcome to lugx</h6>
-                                <h2>BEST GAMING SITE EVER!</h2>
-                                <p>
-                                    LUGX Gaming is free Bootstrap 5 HTML CSS website template for your
-                                    gaming websites. You can download and use this layout for
-                                    commercial purposes. Please tell your friends about TemplateMo.
-                                </p>
-                                <div className="search-input">
-                                    <form id="search" action="#">
-                                        <input
-                                            type="text"
-                                            placeholder="Type Something"
-                                            id="searchText"
-                                            name="searchKeyword"
-                                        />
-                                        <button role="button">Search Now</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 offset-lg-2">
-                            <div className="right-image">
-                                <img src="assets/images/banner-image.jpg" alt="" />
-                                <span className="price">$22</span>
-                                <span className="offer">-40%</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="features">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-3 col-md-6">
-                            <a href="#">
-                                <div className="item">
-                                    <div className="image">
-                                        <img
-                                            src="assets/images/featured-01.png"
-                                            alt=""
-                                            style={{ maxWidth: 44 }}
-                                        />
-                                    </div>
-                                    <h4>Free Storage</h4>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-md-6">
-                            <a href="#">
-                                <div className="item">
-                                    <div className="image">
-                                        <img
-                                            src="assets/images/featured-02.png"
-                                            alt=""
-                                            style={{ maxWidth: 44 }}
-                                        />
-                                    </div>
-                                    <h4>User More</h4>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-md-6">
-                            <a href="#">
-                                <div className="item">
-                                    <div className="image">
-                                        <img
-                                            src="assets/images/featured-03.png"
-                                            alt=""
-                                            style={{ maxWidth: 44 }}
-                                        />
-                                    </div>
-                                    <h4>Reply Ready</h4>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-md-6">
-                            <a href="#">
-                                <div className="item">
-                                    <div className="image">
-                                        <img
-                                            src="assets/images/featured-04.png"
-                                            alt=""
-                                            style={{ maxWidth: 44 }}
-                                        />
-                                    </div>
-                                    <h4>Easy Layout</h4>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="section trending">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-6">
-                            <div className="section-heading">
-                                <h6>Trending</h6>
-                                <h2>Trending Games</h2>
-                            </div>
-                        </div>
-                        <div className="col-lg-6">
-                            <div className="main-button">
-                                <a href="shop.html">View All</a>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-md-6">
-                            <div className="item">
-                                <div className="thumb">
-                                    <a href="product-details.html">
-                                        <img src="assets/images/trending-01.jpg" alt="" />
-                                    </a>
-                                    <span className="price">
-                                        <em>$28</em>$20
-                                    </span>
-                                </div>
-                                <div className="down-content">
-                                    <span className="category">Action</span>
-                                    <h4>Assasin Creed</h4>
-                                    <a href="product-details.html">
-                                        <i className="fa fa-shopping-bag" />
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-md-6">
-                            <div className="item">
-                                <div className="thumb">
-                                    <a href="product-details.html">
-                                        <img src="assets/images/trending-02.jpg" alt="" />
-                                    </a>
-                                    <span className="price">$44</span>
-                                </div>
-                                <div className="down-content">
-                                    <span className="category">Action</span>
-                                    <h4>Assasin Creed</h4>
-                                    <a href="product-details.html">
-                                        <i className="fa fa-shopping-bag" />
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-md-6">
-                            <div className="item">
-                                <div className="thumb">
-                                    <a href="product-details.html">
-                                        <img src="assets/images/trending-03.jpg" alt="" />
-                                    </a>
-                                    <span className="price">
-                                        <em>$64</em>$44
-                                    </span>
-                                </div>
-                                <div className="down-content">
-                                    <span className="category">Action</span>
-                                    <h4>Assasin Creed</h4>
-                                    <a href="product-details.html">
-                                        <i className="fa fa-shopping-bag" />
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-md-6">
-                            <div className="item">
-                                <div className="thumb">
-                                    <a href="product-details.html">
-                                        <img src="assets/images/trending-04.jpg" alt="" />
-                                    </a>
-                                    <span className="price">$32</span>
-                                </div>
-                                <div className="down-content">
-                                    <span className="category">Action</span>
-                                    <h4>Assasin Creed</h4>
-                                    <a href="product-details.html">
-                                        <i className="fa fa-shopping-bag" />
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
+            <Welcome />
+
+            <Features />
+
+            <Trending />
+            
             <div className="section most-played">
                 <div className="container">
                     <div className="row">
@@ -459,19 +239,8 @@ function App() {
                     </div>
                 </div>
             </div>
-            <footer>
-                <div className="container">
-                    <div className="col-lg-12">
-                        <p>
-                            Copyright © 2048 LUGX Gaming Company. All rights reserved.
-                            &nbsp;&nbsp;{" "}
-                            <a rel="nofollow" href="https://templatemo.com" target="_blank">
-                                Design: TemplateMo
-                            </a>
-                        </p>
-                    </div>
-                </div>
-            </footer>
+
+            <Footer />
         </>
 
     )
---
+++ gaming/src/components/Preloader.jsx
@@ -0,0 +1,14 @@
+export default function Preloader() {
+    return (
+        <div id="js-preloader" className="js-preloader">
+            <div className="preloader-inner">
+                <span className="dot" />
+                <div className="dots">
+                    <span />
+                    <span />
+                    <span />
+                </div>
+            </div>
+        </div>
+    );
+}
